ca923a0fd5d66294461515a675e941d4fe08c056
israeljruiz <israeljruiz@gmail.com>
2023-01-17 14:30:26 -0500

    We did it!

---
+++ csharp-prep/Prep3b/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+        string play = "yes";
+
+        while (play = "yes") ;
+        {
+
+        }
+
+    }
+}
