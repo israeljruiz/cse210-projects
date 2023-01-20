aa03866ce229271538df6cf018166610e70d2587
israeljruiz <israeljruiz@gmail.com>
2023-01-20 13:07:28 -0500

    new stuff!

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -7,6 +7,13 @@ public class Job
     public int _startYear;
     public int _endYear;
 
+    public Job(string aCompany, string aJobTitle, int aStartYear)
+    {
+        _company = aCompany;
+        _jobTitle = aJobTitle;
+        _startYear = aStartYear;
+    }
+
     public void Display()
     {
         Console.WriteLine($"{_company} {_jobTitle} {_startYear}-{_endYear}");
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,15 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Job job1 = new Job();
+        Job job1 = new Job("Netflix", "Head", 2023);
         job1._company = "Netflix";
         job1._jobTitle = "Head developer";
         job1._startYear = 2023;
         job1._endYear = 2026;
 
+        Console.WriteLine($"I'm here {job1._company}");
         job1.Display();
 
-        Job job2 = new Job();
+        Job job2 = new Job("Alphabet", "Boss", 2026);
         job2._company = "Alphabet";
         job2._jobTitle = "Security specialist";
         job2._startYear = 2026;
